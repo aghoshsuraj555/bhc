1f4e87f2feeb22d997be65ae553aa66bc0737066
John Allen Berty <johnallenberty95@gmail.com>
2021-09-29 00:58:07 +0530

    added collapse nav link

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -13,19 +13,34 @@
 	</div>
 </header>
 <div class="l-navbar position-fixed vh-100 top-0 show-side" id="nav-bar">
-    <nav class="h-100 d-flex flex-column justify-content-between overflow-hidden">
-        <div>
-            <a href="#" class="nav_logo mb-2"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name fw-bold">BBBootstrap</span> </a>
-            <div class="nav_list">
-                <a href="#" class="nav_link"> <i class='bx bx-grid-alt nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard"></i> <span class="nav_name">Dashboard</span> </a>
-                <a href="<?php echo base_url('branch');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Branch</span> </a>
-                <a href="<?php echo base_url('consultation_status');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Consultation Status"></i> <span class="nav_name">Consultation Status</span> </a>
-				<a href="<?php echo base_url('enquiry');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Enquiries</span> </a>
-                <a href="<?php echo base_url('enquiry_status');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiry Status"></i> <span class="nav_name">Enquiry Status</span> </a>
-                <a href="<?php echo base_url('source');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Source"></i> <span class="nav_name">Source</span> </a>
-                <a href="<?php echo base_url('role');?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Role"></i> <span class="nav_name">Role</span> </a>
-                <a href="#" class="nav_link"> <i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Users"></i> <span class="nav_name">Users</span> </a>
-            </div>
-        </div>
-    </nav>
+	<nav class="h-100 d-flex flex-column justify-content-between overflow-hidden">
+		<div>
+			<a href="#" class="nav_logo mb-2"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name fw-bold">BBBootstrap</span> </a>
+			<div class="nav_list">
+				<ul class="list-unstyled">
+					<li><a href="#" class="nav_link"> <i class='bx bx-grid-alt nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard"></i> <span class="nav_name">Dashboard</span> </a></li>
+					<li><a href="<?php echo base_url('branch'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Branch</span> </a></li>
+					<li><a href="<?php echo base_url('consultation_status'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Consultation Status"></i> <span class="nav_name">Consultation Status</span> </a></li>
+					<li><a href="<?php echo base_url('enquiry'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Enquiries</span> </a></li>
+					<li><a href="<?php echo base_url('enquiry_status'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiry Status"></i> <span class="nav_name">Enquiry Status</span> </a></li>
+					<li><a href="<?php echo base_url('source'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Source"></i> <span class="nav_name">Source</span> </a></li>
+					<li><a href="<?php echo base_url('role'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Role"></i> <span class="nav_name">Role</span> </a></li>
+					<li><a href="#" class="nav_link"> <i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Users"></i> <span class="nav_name">Users</span> </a></li>
+					<li>
+						<a class="nav_link" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+						<i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Users"></i> <span class="nav_name">Home</span> 
+						</a>
+						<div class="collapse" id="home-collapse">
+							<ul class="btn-toggle-nav list-unstyled fw-normal pb-1  ps-5">
+								<li><a href="#" class="nav_link">Overview</a></li>
+								<li><a href="#" class="nav_link">Updates</a></li>
+								<li><a href="#" class="nav_link">Reports</a></li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</nav>
 </div>
