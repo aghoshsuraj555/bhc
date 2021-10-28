be236126005538de660dbfe5ad37996fc7c587ca
John Allen Berty <johnallenberty95@gmail.com>
2021-10-28 23:02:19 +0530

    changed icons

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -24,20 +24,20 @@
 <div class="l-navbar position-fixed vh-100 top-0 show-side" id="nav-bar">
 	<nav class="header-menu vh-100 d-flex flex-column justify-content-between">
 		<div>
-			<a href="#" id="iconId" class="nav_logo icon mb-2"> <img src="<?php echo base_url('public/assets/images/logo-new.png'); ?>" class="img-fluid header-image" alt=""> </a>
+			<a href="#" id="iconId" class="nav_logo icon pt-0 mt-0"> <img src="<?php echo base_url('public/assets/images/logo-new.png'); ?>" class="img-fluid header-image" alt=""> </a>
 			<div class="nav_list">
-				<a href="#" class="nav_link"> <i class='bx bx-grid-alt nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard"></i> <span class="nav_name">Dashboard</span> </a>
-				<a href="<?php echo base_url('enquiry'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Enquiries</span> </a>
-				<a href="<?php echo base_url('patient'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Patients"></i> <span class="nav_name">Patients</span> </a>
-				<a href="<?php echo base_url('branch'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries"></i> <span class="nav_name">Branch</span> </a>
-				<a href="<?php echo base_url('consultation_status'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Consultation Status"></i> <span class="nav_name">Consultation Status</span> </a>
-				<a href="<?php echo base_url('enquiry_status'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiry Status"></i> <span class="nav_name">Enquiry Status</span> </a>
-				<a href="<?php echo base_url('service'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Services"></i> <span class="nav_name">Services</span> </a>
-				<a href="<?php echo base_url('source'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Sources"></i> <span class="nav_name">Sources</span> </a>
-				<a href="<?php echo base_url('role'); ?>" class="nav_link"> <i class='bx bx-message-square-detail nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Roles"></i> <span class="nav_name">Roles</span> </a>
-				<a href="<?php echo base_url('user'); ?>" class="nav_link"> <i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Users"></i> <span class="nav_name">Users</span> </a>
+				<a href="#" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/dashboard.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboard" alt="" srcset=""> <span class="nav_name">Dashboard</span> </a>
+				<a href="<?php echo base_url('enquiry'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/enquiry.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiries" alt="" srcset="">	 <span class="nav_name">Enquiries</span> </a>
+				<a href="<?php echo base_url('patient'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/patient.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Patients" alt="" srcset=""> <span class="nav_name">Patients</span> </a>
+				<a href="<?php echo base_url('branch'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/branch.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Branches" alt="" srcset=""> <span class="nav_name">Branch</span> </a>
+				<a href="<?php echo base_url('consultation_status'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/consulting.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Consultation Status" alt="" srcset=""><span class="nav_name">Consultation Status</span> </a>
+				<a href="<?php echo base_url('enquiry_status'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/search.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Enquiry Status" alt="" srcset=""> <span class="nav_name">Enquiry Status</span> </a>
+				<a href="<?php echo base_url('service'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/customer-service.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Services" alt="" srcset=""> <span class="nav_name">Services</span> </a>
+				<a href="<?php echo base_url('source'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/search-worldwide.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Sources" alt="" srcset=""> <span class="nav_name">Sources</span> </a>
+				<a href="<?php echo base_url('role'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/roles.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Roles" alt="" srcset=""> <span class="nav_name">Roles</span> </a>
+				<a href="<?php echo base_url('user'); ?>" class="nav_link"> <img src="<?php echo base_url('/public/assets/images/icons/user.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Users" alt="" srcset=""> <span class="nav_name">Users</span> </a>
 				<a class="nav_link" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-					<i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Users"></i> <span class="nav_name">Home <i class="bx bx-caret-down"></i></span>
+				<img src="<?php echo base_url('/public/assets/images/icons/settings.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Settings" alt="" srcset=""><span class="nav_name">Settings <i class="bx bx-caret-down"></i></span>
 				</a>
 				<div class="collapse" id="home-collapse">
 					<ul class="btn-toggle-nav list-unstyled fw-normal ps-md-5">
@@ -46,7 +46,7 @@
 						<li><a href="#" class="nav_link">Reports</a></li>
 					</ul>
 				</div>
-				<a href="<?php echo base_url('enquiry/daily_report'); ?>" class="nav_link"> <i class='bx bx-user nav_icon' data-bs-toggle="tooltip" data-bs-placement="right" title="Daily Report"></i> <span class="nav_name">Daily Report</span> </a>
+				<a href="<?php echo base_url('enquiry/daily_report'); ?>" class="nav_link"><img src="<?php echo base_url('/public/assets/images/icons/daily-report.png'); ?> " data-bs-toggle="tooltip" data-bs-placement="right" title="Daily Report" alt="" srcset=""> <span class="nav_name">Daily Report</span> </a>
 				
 			</div>
 		</div>
